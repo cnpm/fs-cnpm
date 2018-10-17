cdfcebded39dd42e672e9dcee0aef3e528f05472
fengmk2 <fengmk2@gmail.com>
2018-10-17 15:19:36 +0800

    refactor: change generator function to async function
    Breaking change

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ fs-cnpm
 [![NPM version][npm-image]][npm-url]
 [![build status][travis-image]][travis-url]
 [![Test coverage][coveralls-image]][coveralls-url]
-[![Gittip][gittip-image]][gittip-url]
 [![David deps][david-image]][david-url]
 
 [npm-image]: https://img.shields.io/npm/v/fs-cnpm.svg?style=flat
@@ -13,8 +12,6 @@ fs-cnpm
 [travis-url]: https://travis-ci.org/cnpm/fs-cnpm
 [coveralls-image]: https://img.shields.io/coveralls/cnpm/fs-cnpm.svg?style=flat
 [coveralls-url]: https://coveralls.io/r/cnpm/fs-cnpm?branch=master
-[gittip-image]: https://img.shields.io/gittip/dead-horse.svg?style=flat
-[gittip-url]: https://www.gittip.com/dead-horse/
 [david-image]: https://img.shields.io/david/cnpm/fs-cnpm.svg?style=flat
 [david-url]: https://david-dm.org/cnpm/fs-cnpm
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,82 +1,76 @@
-/*!
-* fs-cnpm - test/index.test.js
- * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
- * MIT Licensed
- */
-
 'use strict';
 
-/**
- * Module dependencies.
- */
+const FSClient = require('..');
+const path = require('path');
+const fs = require('mz/fs');
+const assert = require('assert');
 
-var FSClient = require('..');
-var path = require('path');
-var fs = require('co-fs');
+const dir = path.join(__dirname, 'dist');
 
-var dir = path.join(__dirname, 'dist');
-
-var client;
-describe('fs-cnpm', function () {
-  describe('create', function () {
-    it('should throw error without options', function () {
-      (function () {
-        FSClient();
-      }).should.throw('need present options.dir');
+let client;
+describe('fs-cnpm', () => {
+  describe('create', () => {
+    it('should throw error without options', () => {
+      assert.throws(() => {
+        new FSClient();
+      }, /need present options\.dir/);
     });
 
-    it('should throw error without options.dir', function () {
-      (function () {
-        FSClient({});
-      }).should.throw('need present options.dir');
+    it('should throw error without options.dir', () => {
+      assert.throws(() => {
+        new FSClient({});
+      }, /need present options\.dir/);
     });
 
-    it('should create client ok', function () {
-      client = FSClient({dir: dir});
-      client.dir.should.equal(dir);
+    it('should create client ok', () => {
+      client = new FSClient({ dir });
+      assert(client.dir === dir);
     });
   });
 
-  describe('uploadBuffer()', function () {
-    it('should upload ok', function* () {
-      var res = yield client.uploadBuffer('hello', {key: 'hello'});
-      res.key.should.equal('hello');
-      (yield fs.readFile(path.join(dir, 'hello'), 'utf8')).should.equal('hello');
+  describe('uploadBuffer()', () => {
+    it('should upload ok', async () => {
+      let res = await client.uploadBuffer('hello', { key: 'hello/bar.tgz' });
+      assert(res.key === 'hello/bar.tgz');
+      assert.equal(await fs.readFile(path.join(dir, res.key), 'utf8'), 'hello');
 
-      var res = yield client.uploadBuffer('hello', {key: '/a/b/c/d/e/f/g.txt'});
-      res.key.should.equal('/a/b/c/d/e/f/g.txt');
-      (yield fs.readFile(path.join(dir, '/a/b/c/d/e/f/g.txt'), 'utf8')).should.equal('hello');
+      res = await client.uploadBuffer('hello1', { key: '/a/b/c/d/e/f/g.txt' });
+      assert(res.key === '/a/b/c/d/e/f/g.txt');
+      assert.equal(await fs.readFile(path.join(dir, res.key), 'utf8'), 'hello1');
 
-      var res = yield client.uploadBuffer('hello', {key: 'foo/-/foo-1.3.2.txt'});
-      res.key.should.equal('foo/-/foo-1.3.2.txt');
-      (yield fs.readFile(path.join(dir, 'foo/-/foo-1.3.2.txt'), 'utf8')).should.equal('hello');
+      res = await client.uploadBuffer('hello2', { key: '/foo/-/foo-1.3.2.txt' });
+      assert(res.key === '/foo/-/foo-1.3.2.txt');
+      assert.equal(await fs.readFile(path.join(dir, res.key), 'utf8'), 'hello2');
     });
   });
 
-  describe('upload()', function () {
-    it('should upload ok', function* () {
-      var res = yield client.upload(path.join(dir, 'hello'), {key: 'hello1'});
-      res.key.should.equal('hello1');
-      (yield fs.readFile(path.join(dir, 'hello1'), 'utf8')).should.equal('hello');
+  describe('upload()', () => {
+    it('should upload ok', async () => {
+      const res = await client.upload(__filename, { key: 'hello/upload.js' });
+      assert(res.key === 'hello/upload.js');
+      assert.equal(await fs.readFile(path.join(dir, res.key), 'utf8'), await fs.readFile(__filename, 'utf8'));
     });
   });
 
-  describe('download()', function () {
-    it('should download ok', function* () {
-      var dest = path.join(dir, 'world');
-      yield client.download('hello', dest);
-      (yield fs.readFile(path.join(dir, 'world'), 'utf8')).should.equal('hello');
-      yield fs.unlink(dest);
+  describe('download()', () => {
+    it('should download ok', async () => {
+      await client.uploadBuffer('hello bar', { key: 'hello/download-bar.tgz' });
+      const dest = path.join(dir, 'world');
+      await client.download('hello/download-bar.tgz', dest);
+      assert.equal(await fs.readFile(path.join(dir, 'world'), 'utf8'), 'hello bar');
+      await fs.unlink(dest);
     });
   });
 
-  describe('remove()', function () {
-    it('should remove ok', function* () {
-      yield client.remove('hello');
-      yield client.remove('hello1');
-      yield client.remove('foo/-/foo-1.3.2.txt');
-      (yield fs.exists(path.join(dir, 'hello'))).should.equal(false);
-      (yield fs.exists(path.join(dir, 'hello1'))).should.equal(false);
+  describe('remove()', () => {
+    it('should remove ok', async () => {
+      await client.uploadBuffer('hello bar', { key: 'hello/download-bar.tgz' });
+      await client.uploadBuffer('hello bar', { key: '/foo/-/foo-1.3.2.txt' });
+      await client.remove('hello/download-bar.tgz');
+      await client.remove('/foo/-/foo-1.3.2.txt');
+      await client.remove('not-exists-dir/foo/-/foo-1.3.2.txt');
+      assert.equal(await fs.exists(path.join(dir, 'hello/download-bar.tgz')), false);
+      assert.equal(await fs.exists(path.join(dir, 'foo/-/foo-1.3.2.txt')), false);
     });
   });
 });
